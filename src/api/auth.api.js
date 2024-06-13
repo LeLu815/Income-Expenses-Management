b094e2b62a4f4a70ef0a23d69cf63f6c0f6674e0
LinQuixote <leein9404@naver.com>
2024-06-13 12:26:16 +0900

    feat : 로그인 api 셋팅완료

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,20 +1,52 @@
 class AuthApi {
   #client;
+  #accessToken;
   constructor(client) {
     this.#client = client;
+    // 인터셉터 설정
+    this.#client.interceptors.request.use((config) => {
+      if (this.#accessToken) {
+        config.headers.Authorization = `Bearer ${this.#accessToken}`;
+      }
+      return config;
+    });
   }
+
   async login(id, password) {
     const response = await this.#client.post("/login", {
-      id,
-      password,
+      data: { id, password },
     });
+    this.#accessToken = response.accessToken;
+    return response;
   }
+
   async join(id, password, nickname) {
     const response = await this.#client.post("/register", {
-      id,
-      password,
-      nickname,
+      data: { id, password, nickname },
     });
+    return response;
+  }
+
+  async getUserInfo() {
+    const response = await this.#client.get("/user", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response;
+  }
+
+  async patchUserInfo(avatar, nickname) {
+    const response = await this.#client.get("/profile", {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: {
+        avatar,
+        nickname,
+      },
+    });
+    return response;
   }
 }
 
